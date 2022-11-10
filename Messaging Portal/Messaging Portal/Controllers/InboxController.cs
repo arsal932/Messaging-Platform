d92f8e621d8109364395d1fd01676fbb18207d9c
Shah Fahad Ahmad <sfahad566@gmail.com>
2022-11-11 01:13:39 +0500

    Admin Tempate Added, Created Layout, created inbox font-end page(incomplete)

---
+++ Messaging Portal/Messaging Portal/Controllers/InboxController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Messaging_Portal.Controllers;
+public class InboxController : Controller
+{
+    public InboxController()
+    {
+
+    }
+    public async Task<IActionResult> Chats()
+    {
+        return View();
+    }
+}
