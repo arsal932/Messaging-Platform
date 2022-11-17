5f9948a2c35800cef86209a4d2daa0f5e44adc86
Shah Fahad Ahmad <sfahad566@gmail.com>
2022-11-18 01:13:57 +0500

    - Added Compaigns Module UI. - Update Signup form UI.

---
+++ Messaging Portal/Messaging Portal/Controllers/CompaignsController.cs
@@ -0,0 +1,38 @@
+﻿using Messaging_Portal.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Messaging_Portal.Controllers
+{
+    public class CompaignsController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            ViewBag.Page="Compaigns";
+            return PartialView();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(CompaignsViewModel compaignsViewModel)
+        {
+            return PartialView();
+        }
+        [HttpGet]
+        public async Task<IActionResult> Compaigns()
+        {
+            ViewBag.page = "Compaigns";
+            return View();
+        }
+        public async Task<IActionResult> GetCompaigns(string search)
+        {
+            Thread.Sleep(2000);
+            return PartialView();
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetCompaignMessage(int CompaignID)
+        {
+            Thread.Sleep(2000);
+            ViewBag.CompaignID = CompaignID;
+            return PartialView();
+        }
+    }
+}
---
+++ Messaging Portal/Messaging Portal/Models/CompaignsViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Messaging_Portal.Models
+{
+    public class CompaignsViewModel
+    {
+        public string CompaignName { get; set; }
+        public int Type { get; set; }
+        public string Message { get; set; }
+        public string OptOutMessage { get; set; }
+        public string FallBackMessage { get; set; }
+        public string FallBackOptOutMessage { get; set; }
+        public string Subject { get; set; }
+    }
+}
