99a214e705a1e1374aff96e584e554a83ad3a2fd
Shah Fahad Ahmad <sfahad566@gmail.com>
2022-11-21 23:13:16 +0500

    - Dashboard UI Added and Compaigns updated.

---
+++ Messaging Portal/Messaging Portal/Controllers/DashboardController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Messaging_Portal.Controllers
+{
+    public class DashboardController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> view()
+        {
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetBarChart()
+        {
+            return PartialView();
+        }
+    }
+}
